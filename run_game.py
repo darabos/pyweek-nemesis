db7530b2e87d146d2f3ffb87c912fede48a8e622
Daniel Darabos <darabos.daniel@gmail.com>
2013-04-14 17:11:50 +0100

    Dialog.

--- run_game.py
+++ run_game.py
@@ -1,9 +1,11 @@
+# coding: utf8
 import math
 import pygame
 import random
 import sys
 from OpenGL.GL import *
 
+WIDTH, HEIGHT = 600.0, 600.0
 
 def ShapeFromMouseInput(mouse_path, crystals):
   """
@@ -47,12 +49,9 @@ def DrawCrystal(x, y, width, height):
     width, height: Width and height of crystal.
   """
   # Placeholder.
-  glBindBuffer(GL_ARRAY_BUFFER, Crystal.vbo.id)
-  glEnableClientState(GL_VERTEX_ARRAY)
-  glVertexPointer(3, GL_FLOAT, 0, None)
   glPushMatrix()
   glTranslatef(x, y, 0)
-  glDrawArrays(GL_QUADS, 0, 4)
+  Crystal.vbo.Render()
   glPopMatrix()
 
 def ShipPathFromWaypoints(starting_location, starting_velocity, waypoints, acceleration=1):
@@ -100,7 +99,6 @@ def ShipPathFromWaypoints(starting_location, starting_velocity, waypoints, accel
     return waypoints[-1]
 
   def control(time):
-    time *= 0.001
     distance = 0
     velocity = 0
     if time < 0:
@@ -128,15 +126,25 @@ def ShipPathFromWaypoints(starting_location, starting_velocity, waypoints, accel
   return control
 
 
-
 class Quad(object):
+
   def __init__(self, w, h):
-    self.buf = (ctypes.c_float * (4 * 3))()
-    for i, x in enumerate([-w/2, -h/2, 0, w/2, -h/2, 0, w/2, h/2, 0, -w/2, h/2, 0]):
+    self.buf = (ctypes.c_float * (4 * 5))()
+    for i, x in enumerate([-w/2, -h/2, 0, 0, 0, w/2, -h/2, 0, 1, 0, w/2, h/2, 0, 1, 1, -w/2, h/2, 0, 0, 1]):
       self.buf[i] = x
     self.id = glGenBuffers(1)
     glBindBuffer(GL_ARRAY_BUFFER, self.id)
     glBufferData(GL_ARRAY_BUFFER, ctypes.sizeof(self.buf), self.buf, GL_STATIC_DRAW)
+
+  def Render(self):
+    F = ctypes.sizeof(ctypes.c_float)
+    FP = lambda x: ctypes.cast(x * F, ctypes.POINTER(ctypes.c_float))
+    glBindBuffer(GL_ARRAY_BUFFER, self.id)
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+    glVertexPointer(3, GL_FLOAT, 5 * F, FP(0))
+    glTexCoordPointer(2, GL_FLOAT, 5 * F, FP(3))
+    glDrawArrays(GL_QUADS, 0, 4)
 
 
 class Crystal(object):
@@ -156,19 +164,165 @@ class Ship(object):
     self.y = y
     self.vbo = Quad(size, size)
   def Render(self):
-    glBindBuffer(GL_ARRAY_BUFFER, self.vbo.id)
-    glEnableClientState(GL_VERTEX_ARRAY)
-    glVertexPointer(3, GL_FLOAT, 0, None)
     glPushMatrix()
     glTranslatef(self.x, self.y, 0)
-    glDrawArrays(GL_QUADS, 0, 4)
+    self.vbo.Render()
     glPopMatrix()
+
+class Texture(object):
+  def __init__(self, surface):
+    data = pygame.image.tostring(surface, 'RGBA', 1)
+    self.id = glGenTextures(1)
+    self.width = surface.get_width()
+    self.height = surface.get_height()
+    glBindTexture(GL_TEXTURE_2D, self.id)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.width, self.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
+    self.width /= WIDTH
+    self.height /= HEIGHT
+  def Delete(self):
+    glDeleteTextures(self.id)
+  def __enter__(self):
+    glBindTexture(GL_TEXTURE_2D, self.id)
+    glEnable(GL_TEXTURE_2D)
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    glDisable(GL_TEXTURE_2D)
+
+
+class DialogLine(object):
+  textures = {}
+  side = 'left'
+  quad = None
+  def __init__(self, text, label='', face='', trigger=None):
+    self.text = text
+    self.label = label
+    self.face = face
+    self.trigger = trigger
+  def RenderFace(self):
+    if DialogLine.quad is None:
+      DialogLine.quad = Quad(1.0, 1.0)
+    texture = self.GetTexture()
+    with texture:
+      glPushMatrix()
+      if self.side == 'left':
+        glTranslate(-1 + texture.width / 1.5, -1 + texture.height, 0)
+      elif self.side == 'right':
+        glTranslate(1 - texture.width / 1.5, -1 + texture.height, 0)
+      glScale(texture.width, texture.height, 1)
+      self.quad.Render()
+      glPopMatrix()
+  def GetTexture(self):
+    filename = self.character
+    if self.face:
+      filename += '-' + self.face
+    filename += '.png'
+    if filename not in self.textures:
+      self.textures[filename] = Texture(pygame.image.load(filename))
+    return self.textures[filename]
+
+class Father(DialogLine):
+  character = 'Father'
+class Kid(DialogLine):
+  character = 'Kid'
+  side = 'right'
+class Jellyfish(DialogLine):
+  character = 'Jellyfish'
+
+
+class Dialog(object):
+  dialog = [
+    Father(u'Here we are, my son. The Sea of Good and Bad.',
+           label='here-we-are', trigger=lambda game: game.time > 1),
+    Father(u'Get in the Needle and let’s collect some Mana!'),
+    Kid(u'I get to control the Needle?!', face='wonder'),
+    Father(u'That’s right. Just draw lines with the mouse and the Needle will follow them.'),
+    Father(u'We’re here to collect Mana, remember?', label='to-collect-mana',
+           trigger=lambda game: game.lines_drawn > 2),
+    Father(u'Weave the Needle through three white crystals to form a triangle, will you?'),
+    Father(u'Well done. That’s a perfect triangle!', label='just-right-click',
+           trigger=lambda game: game.charging),
+    Father(u'Perfect shapes provide the most Mana.'),
+    Father(u'Let’s wait a bit for it to fully charge. Let me know when it’s ready.'),
+    Father(u'Just right click on the shape and I’ll come and haul it in.'),
+    Father(u'Oh, your mother will summon us a delicious dinner...',
+           label='oh-your-mother', face='laughing',
+           trigger=lambda game: game.mana > 0),
+    Father(u'...using this Mana when we get home.'),
+    Father(u'Let us collect at least 1,000,000 so it feeds the whole family!'),
+    Kid(u'Look, jellyfish! Can they speak?', face='wonder', label='look-jellyfish',
+        trigger=lambda game: game.mana >= 10000),
+    Jellyfish(u'Yes we can, tasty human!'),
+    Father(u'Keep the Needle away from them! I will handle these beasts.'),
+    Kid(u'What was that? A ship under the water snatched our Mana!',
+        label='what-was-that', face='scared',
+        trigger=lambda game: False),
+    Father(u'It’s an Undership!'),
+    Father(u'On the Sea of Good and Bad our reflections have their own minds.'),
+    Father(u'And they will steal our dinner if we let them!'),
+    Father(u'GAME OVER', label='game-over', trigger=lambda game: False),  # Sentinel.
+    ]
+
+  def __init__(self):
+    self.state = self.State('here-we-are')
+    self.paused = False
+    self.texture = None
+    self.quad = Quad(1.0, 1.0)
+    pygame.font.init()
+    self.font = pygame.font.Font('OpenSans-Regular.ttf', 30)
+
+  def State(self, label):
+    for i, d in enumerate(self.dialog):
+      if d.label == label:
+        return i
+    raise ValueError('Label {} not found.'.format(label))
+
+  def Pause(self, game):
+    dialog = self.dialog[self.state]
+    if self.paused:
+      for e in pygame.event.get():
+        if e.type == pygame.KEYUP or e.type == pygame.MOUSEBUTTONUP:
+          self.state += 1
+          self.texture.Delete()
+          self.texture = None
+          if self.dialog[self.state].label:
+            self.paused = False
+    else:
+      if dialog.trigger(game):
+        self.paused = True
+
+  def RenderFont(self, text, antialias, color, background):
+    return self.font.render(text, antialias, color, background)
+  def Render(self):
+    if not self.paused:
+      return
+    dialog = self.dialog[self.state]
+    if not self.texture:
+      self.texture = Texture(self.RenderFont(dialog.text, antialias=True, color=(0, 0, 0), background=(255, 255, 255)))
+    # White block.
+    glPushMatrix()
+    glTranslate(0, -0.9, 0)
+    glScale(2, 0.2, 1)
+    self.quad.Render()
+    glPopMatrix()
+    # Face.
+    dialog.RenderFace()
+    # Text.
+    with self.texture:
+      glPushMatrix()
+      glTranslate(0, -0.9, 0)
+      glScale(self.texture.width, self.texture.height, 1)
+      self.quad.Render()
+      glPopMatrix()
 
 
 class Game(object):
 
   def __init__(self):
     self.objects = []
+    self.charging = []
+    self.lines_drawn = 0
+    self.mana = 0
 
   def Start(self):
     self.Init()
@@ -176,11 +330,11 @@ class Game(object):
 
   def Init(self):
     pygame.init()
-    self.width, self.height = 600, 600
-    pygame.display.set_mode((self.width, self.height), pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE)
+    pygame.display.set_mode((int(WIDTH), int(HEIGHT)), pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE)
     pygame.display.set_caption('Nemesis')
-    glViewport(0, 0, self.width, self.height)
+    glViewport(0, 0, int(WIDTH), int(HEIGHT))
 
+    self.dialog = Dialog()
     for i in range(100):
       self.objects.append(Crystal(random.uniform(-1, 1), random.uniform(-1, 1)))
     self.small_ship = Ship(0, 0, 0.05)
@@ -195,19 +349,20 @@ class Game(object):
 
   def Loop(self):
     clock = pygame.time.Clock()
-    time = 0
+    self.time = 0
     while True:
       dt = clock.tick()
-      time += dt
-      self.Update(dt, time)
+      if not self.dialog.Pause(self):
+        self.Update(dt)
       glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT)
       self.DrawPath(self.small_ship.drawing)
       for o in self.objects:
         o.Render()
+      self.dialog.Render()
       pygame.display.flip()
 
   def GameSpace(self, x, y):
-    w, h = float(self.width), float(self.height)
+    w, h = WIDTH, HEIGHT
     return 2 * x / w - 1, 1 - 2 * y / h
 
   def DrawPath(self, path):
@@ -228,18 +383,20 @@ class Game(object):
       ly = y
     glEnd()
 
-  def Update(self, dt, time):
+  def Update(self, dt):
     dt *= 0.001
+    self.time += dt
     for e in pygame.event.get():
       if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
         pygame.quit()
         sys.exit(0)
       if e.type == pygame.MOUSEBUTTONUP and e.button == 3:
         self.big_ship.path_func = ShipPathFromWaypoints((self.big_ship.x, self.big_ship.y), (0, 0), [self.GameSpace(*e.pos)])
-        self.big_ship.path_func_start_time = time
+        self.big_ship.path_func_start_time = self.time
       if e.type == pygame.MOUSEBUTTONUP and e.button == 1:
         self.small_ship.path_func = ShipPathFromWaypoints((self.small_ship.x, self.small_ship.y), (0, 0), self.small_ship.drawing, 10)
-        self.small_ship.path_func_start_time = time
+        self.small_ship.path_func_start_time = self.time
+        self.lines_drawn += 1
       if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
         self.small_ship.drawing = [self.GameSpace(*e.pos)]
       if e.type == pygame.MOUSEMOTION and e.buttons[0]:
@@ -247,7 +404,7 @@ class Game(object):
 
     for ship in [self.small_ship, self.big_ship]:
       if ship.path_func:
-        (x, y, dx, dy) = ship.path_func(time - ship.path_func_start_time)
+        (x, y, dx, dy) = ship.path_func(self.time - ship.path_func_start_time)
         ship.x = x
         ship.y = y
 
