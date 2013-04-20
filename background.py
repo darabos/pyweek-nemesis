928f495b480a610f10e6a3b6b78611367b17c198
nchriz <chma00@gmail.com>
2013-04-20 16:30:26 +0200

    Shaders, Background for water
    Started on shaders for the crystal.
    Started on a background quad for water effect.

---
+++ background.py
@@ -0,0 +1,25 @@
+import OpenGL
+from OpenGL.GL import *
+import assets
+
+class BackGround(object):
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def Draw(self):
+        glUseProgram(assets.BACKGROUND_PROGRAM)
+
+        location = glGetUniformLocation(assets.BACKGROUND_PROGRAM, 'color')
+        glUniform4f(location, 0., 0., 0., 0.)
+
+        #glColor(self.color[0], self.color[1], self.color[2])
+        glBegin(GL_QUADS)
+        glVertex(self.x[0], self.y[1])
+        glVertex(self.x[0], self.y[0])
+        glVertex(self.x[1], self.y[0])
+        glVertex(self.x[1], self.y[1])
+        glEnd()
+
+        glUseProgram(0)
