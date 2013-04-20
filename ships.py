bad547dbae049d545c57af0483ab88ba8ba5e741
Zsombor Nagy <zsombor@nagy.com>
2013-04-21 01:00:40 +0200

    Big ships should start with zero mana

--- ships.py
+++ ships.py
@@ -188,7 +188,7 @@ class BigShip(MeshShip):
   id = 0
   def __init__(self, x, y, size):
     super(BigShip, self).__init__(x, y, size, assets.Meshes.ship)
-    self.mana = 100.0
+    self.mana = 0.0
     self.health = 10.0
     self.max_health = 10.0
     self.name = 'Big Ship %i' % BigShip.id
